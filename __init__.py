9b062eab2d6e5d95a5f7a09b6ceaee0a0f046bf1
noircop <120275216+noircop@users.noreply.github.com>
2026-06-11 15:28:31 +0300

    Connecting Context
    Connecting Context and clearing store and imports

--- __init__.py
+++ __init__.py
@@ -1,9 +1,2 @@
-from .core.flow_controller import FlowController
-
-from .core.character_model import CharacterModel
-from .core.character_service import CharacterService
-from .core.character_validator import CharacterValidator
-
-from .core.image_loader import ImageLoader
-
 from .core.editor_facade import EditorFacade
+from .core.editor_context import EditorContext
